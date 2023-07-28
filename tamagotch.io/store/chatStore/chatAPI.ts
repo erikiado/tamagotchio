c948226c4e11bf139e07faf6312fb857e5478c19
erikiado <erikiado@gmail.com>
2023-07-28 15:33:11 -0700

    log env variable

--- tamagotch.io/store/chatStore/chatAPI.ts
+++ tamagotch.io/store/chatStore/chatAPI.ts
@@ -6,7 +6,7 @@ export const currentEnv = process.env.NODE_ENV === "production" ? "dev" : "local
 import { CHAT_SERVICE } from '../../constants';
 
 export async function chatRequest(prompt:string): Promise<{ answer: any }> {
-  console.log({CHAT_SERVICE},"chatservice", process.env.CHAT_SERVICE, process.env)
+  console.log({CHAT_SERVICE},"chatservice", process.env.CHAT_SERVICE, process.env, process.env.NODE_ENV, 'NEXT_PUBLIC_CHAT_SERVICE',process.env.NEXT_PUBLIC_CHAT_SERVICE)
   console.log("requesting chat...")
   const response = await axiosApi.post(CHAT_SERVICE, {prompt});
   console.log({response});
