08400fb7f51881612de74a9c15789f150bebc270
erikiado <erikiado@gmail.com>
2023-07-28 14:13:52 -0700

    get chat service from env

--- tamagotch.io/store/chatStore/chatAPI.ts
+++ tamagotch.io/store/chatStore/chatAPI.ts
@@ -4,9 +4,10 @@ import axiosApi, { parseFiberResponse } from '../../services/AxiosInstance';
 import axios from 'axios';
 
 // import { ADMIN_SERVICE, CHAT_SERVICE } from '../../constants';
-const CHAT_SERVICE = process.env.CHAT_SERVICE || "http://127.0.0.1:5000/";
+const CHAT_SERVICE = process.env.CHAT_SERVICE ? process.env.CHAT_SERVICE : "http://127.0.0.1:5000/";
 
 export async function chatRequest(prompt:string): Promise<{ answer: any }> {
+  console.log({CHAT_SERVICE})
   console.log("requesting chat...")
   const response = await axiosApi.post(CHAT_SERVICE, {prompt});
   console.log({response});
